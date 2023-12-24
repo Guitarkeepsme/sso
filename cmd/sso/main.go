abe283c82df09a3f5a58f63bc3ef40960de653d3
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-12-24 06:54:45 +0300

    setup logger in main function

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
 )
 
@@ -9,6 +11,22 @@ func main() {
 	cfg := config.MustLoad()
 
 	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting application",
+		slog.String("env", cfg.Env),
+		slog.String("storage_path", cfg.StoragePath),
+		slog.String("token_ttl", cfg.TokenTTL.String()),
+		slog.Int("port", cfg.GRPC.Port),
+		slog.String("timeout", cfg.GRPC.Timeout.String()))
+
+	log.Debug("debug message")
+
+	log.Error("error message")
+
+	log.Warn("warning message")
+
 	// ToDo: инициализировать объект конфигурации
 
 	// ToDo: инициализировать логгер
@@ -16,4 +34,25 @@ func main() {
 	// ToDo: запустить приложение
 
 	// ToDo: запустить сервер
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case "local":
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case "dev":
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case "prod":
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+
+	}
+	return log
 }
