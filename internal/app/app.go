65ed04851e2c51e4633700ae41674c0bce5bff96
Алексей Кашурников <alexeykashurnikov@gmail.com>
2024-01-06 17:37:11 +0300

    app files updated

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,28 +1,7 @@
 package app
 
-import (
-	"log/slog"
-
-	authgrpc "sso/internal/grpc/auth"
-
-	"google.golang.org/grpc"
-)
+import "google.golang.org/grpc"
 
 type App struct {
-	log        *slog.Logger
 	gRPCServer *grpc.Server
-	port       int
-}
-
-// New creates a new App
-func New(log *slog.Logger, port int) *App {
-	gRPCServer := grpc.NewServer()
-
-	authgrpc.Register(gRPCServer)
-
-	return &App{
-		log:        log,
-		port:       port,
-		gRPCServer: gRPCServer,
-	}
 }
--- internal/app/grpc/app.go
+++ internal/app/grpc/app.go
@@ -1,1 +1,28 @@
 package grpc
+
+import (
+	"log/slog"
+
+	authgrpc "sso/internal/grpc/auth"
+
+	"google.golang.org/grpc"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+// New creates a new App
+func New(log *slog.Logger, port int) *App {
+	gRPCServer := grpc.NewServer()
+
+	authgrpc.Register(gRPCServer)
+
+	return &App{
+		log:        log,
+		port:       port,
+		gRPCServer: gRPCServer,
+	}
+}
